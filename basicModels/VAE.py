a5a4d7975a5d0a8778897b7459ab846b94c043a6
eivskeiv <eivinkoh@stud.ntnu.no>
2022-05-24 15:52:35 +0200

    VAE with working tensorboard to track progress

--- basicModels/VAE.py
+++ basicModels/VAE.py
@@ -3,59 +3,81 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class VAE(nn.Module):
-    def __init__(self, latent_size):
-        super().__init__()
+class Encoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(Encoder, self).__init__()
+        self.latent_dim = latent_dim
 
-        self.latent_size = latent_size
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, stride=2)  # -> [1, 8, 13, 13]
+        self.conv1_bn = nn.BatchNorm2d(8)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=2)  # -> [1, 16, 6, 6]
+        self.conv2_bn = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, stride=2)  # -> [1, 32, 2, 2]
+        self.conv3_bn = nn.BatchNorm2d(32)
 
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2)
-
-        self.mean = nn.Linear(32 * 6 * 6, self.latent_size)
-        self.logvar = nn.Linear(32 * 6 * 6, self.latent_size)
-
-        self.linear1 = nn.Linear(self.latent_size, 32 * 6 * 6)
-        self.deconv1 = nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2)  # scale up to 16x13x13
-        self.deconv2 = nn.ConvTranspose2d(16, 8, kernel_size=3, stride=2)  # scale up to 8x27x27
-        self.deconv3 = nn.ConvTranspose2d(8, 1, kernel_size=2, stride=1)  # scale up to 8x28x28
+        self.mean = nn.Linear(32 * 2 * 2, self.latent_dim)
+        self.logvar = nn.Linear(32 * 2 * 2, self.latent_dim)
 
     def forward(self, x):
         x = x.view(-1, 1, 28, 28)
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        x = F.relu(self.conv2_bn(self.conv2(x)))
+        x = F.relu(self.conv3_bn(self.conv3(x)))
         x = x.view(x.size(0), -1)
         mean = self.mean(x)
         logvar = self.logvar(x)
+        return mean, logvar
 
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(Decoder, self).__init__()
+        self.latent_dim = latent_dim
+
+        self.linear1 = nn.Linear(self.latent_dim, 32 * 2 * 2)
+
+        self.deconv1 = nn.ConvTranspose2d(32, 16, kernel_size=3, stride=3)  # -> [1, 16, 6, 6]
+        self.deconv1_bn = nn.BatchNorm2d(16)
+        self.deconv2 = nn.ConvTranspose2d(16, 8, kernel_size=3, stride=2, padding=0)  # -> [1, 8, 13, 13]
+        self.deconv2_bn = nn.BatchNorm2d(8)
+        self.deconv3 = nn.ConvTranspose2d(8, 1, kernel_size=3, stride=2, padding=0, output_padding=1)  # -> [1, 1, 28, 28]
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = x.view(-1, 32, 2, 2)
+        x = F.relu(self.deconv1_bn(self.deconv1(x)))
+        x = F.relu(self.deconv2_bn(self.deconv2(x)))
+        x = F.sigmoid(self.deconv3(x))
+
+        return x.view(-1, 28, 28)
+
+
+class VariationalAutoencoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(VariationalAutoencoder, self).__init__()
+
+        self.latent_dim = latent_dim
+
+        self.encoder = Encoder(self.latent_dim)
+        self.decoder = Decoder(self.latent_dim)
+
+    def forward(self, x):
+        mean, logvar = self.encoder(x)
         z = self.reparameterize(mean, logvar)
-        x = self.linear1(z)
-        x = x.view(-1, 32, 6, 6)
-        x = F.relu(self.deconv1(x))
-        x = F.relu(self.deconv2(x))
-        x = F.sigmoid(self.deconv3(x))
-        x = x.view(-1, 28, 28)
 
-        return x, mean, logvar
-
-    def decode(self, z):
-        x = self.linear1(z)
-        x = x.view(-1, 32, 6, 6)
-        x = F.relu(self.deconv1(x))
-        x = F.relu(self.deconv2(x))
-        x = F.sigmoid(self.deconv3(x))
-        x = x.view(-1, 28, 28)
-        return x
-
+        return self.decoder(z), mean, logvar
 
     def reparameterize(self, mean, logvar):
         std = torch.exp(0.5 * logvar)
-        eps = torch.randn(self.latent_size)
-        return mean + eps * std
+        eps = torch.randn(self.latent_dim)
+        return eps.mul(std).add_(mean)
+
+    def decode(self, z):
+        return self.decoder(z)
 
 
 def kullbeck_leibler_divergence(mean, logvar):
-    return -0.5 * torch.sum(mean ** 2 + torch.exp(logvar) - logvar - 1)
+    return 0.5 * torch.sum(mean ** 2 + torch.exp(logvar) - logvar - 1)
 
 
 def reconstruction_loss(target, preds):
