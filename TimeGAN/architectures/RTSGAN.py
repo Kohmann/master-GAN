3cede323ad8a97e61b435aa0f688cd158e92cf38
Kohmann <eivind.kohmann@gmail.com>
2022-11-20 12:25:40 +0000

    save

--- TimeGAN/architectures/RTSGAN.py
+++ TimeGAN/architectures/RTSGAN.py
@@ -143,17 +143,17 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         if SN:
             self.model = nn.Sequential(
-                nn.utils.parametrizations.spectral_norm(nn.Linear((2 * input_dim) // 3, (2 * input_dim) // 3)),
+                nn.utils.parametrizations.spectral_norm(nn.Linear(input_dim, (2 * input_dim) // 3)),
                 nn.LeakyReLU(0.2),
-                nn.utils.parametrizations.spectral_norm(nn.Linear(input_dim // 3, input_dim // 3)),
+                nn.utils.parametrizations.spectral_norm(nn.Linear((2 * input_dim) // 3, input_dim // 3)),
                 nn.LeakyReLU(0.2),
                 nn.Linear(input_dim // 3, 1),
             )
         else:
             self.model = nn.Sequential(
-                nn.Linear((2 * input_dim) // 3, (2 * input_dim) // 3),
+                nn.Linear(input_dim, (2 * input_dim) // 3),
                 nn.LeakyReLU(0.2),
-                nn.Linear(input_dim // 3, input_dim // 3),
+                nn.Linear((2 * input_dim) // 3, input_dim // 3),
                 nn.LeakyReLU(0.2),
                 nn.Linear(input_dim // 3, 1),
             )
